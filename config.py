c864df9f391555e91cdae2e5e2a7ec7cef383e48
mbappe29psg <miguelavs1029@hotmail.com>
2024-06-18 12:42:53 -0600

    Cambio de ruta de la base de datos usuario y contraseña

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ class Config:
     DEBUG = True
     SECRET_KEY = 'e290235e31eec44eeb4ad58137c42d50'
     FLASK_ENV = "development"
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:05534DM1N15TR4D0R@localhost/db_catalogos'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/db_catalogos'
     SQLALCHEMY_BINDS = {
-            'db2': 'mysql://root:05534DM1N15TR4D0R@localhost/db_sia'
+            'db2': 'mysql://root:root@localhost/db_sia'
         }
 
     # Desactivar los eventos Flask-SQLALCHEMY
